5fac7edbd1efb7cdb2e3913c108bb5d65084efd7
mael <emmanuel.ballery@gmail.com>
2013-12-07 09:47:44 +0100

    Fix bundle root class name

---
+++ EBImageMagickBundle.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace EB\ImageMagickBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Class EBImageMagickBundle
+ *
+ * @author "Emmanuel BALLERY" <emmanuel.ballery@gmail.com>
+ */
+class EBImageMagickBundle extends Bundle
+{
+}
